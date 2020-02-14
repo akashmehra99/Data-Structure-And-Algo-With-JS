db411eec2399d108e0ec7974bc380a0dc7917753
Akash Mehra <akash.mehra99@gmail.com>
2020-02-14 19:37:59 +0530

    Checking if a number is a happy number or not

---
+++ ProblemSolving/happy-numbers.js
@@ -0,0 +1,29 @@
+/*
+    Happy number is defined by the below rules:
+        1 -> Starting with nay positive integer, replace the number by sum of squares of it's digits.
+        2 -> Repeat the process until it's equal to 1.
+        3 -> If it runs endlessly it's not a happy number
+*/
+
+let sumOfDigits = {};
+
+const getSquareOfNumber = (number) => {
+    let sumOfSquares = 0;
+    while(number > 0) {
+        const digit = number % 10; 
+        number = parseInt((number / 10), 10);
+        sumOfSquares = sumOfSquares + (digit * digit);
+    }
+    if (sumOfSquares === 1) {
+        return true;
+    } else {
+        if (sumOfDigits.hasOwnProperty(sumOfSquares)) {
+            return false;
+        } else {
+            sumOfDigits[sumOfSquares] = sumOfSquares;
+            return getSquareOfNumber(sumOfSquares);
+        }
+    }
+};
+const number = 19;
+console.log(number, ' - result for happy number :', getSquareOfNumber(number));
