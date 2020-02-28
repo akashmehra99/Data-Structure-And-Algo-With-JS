086fafd3aee2957a88cfe8165c899133293db78e
Akash Mehra <akash.mehra99@gmail.com>
2020-02-28 11:50:23 +0530

    Added BFS in graph ds

--- ProblemSolving/DataStructures/Graphs/graph.js
+++ ProblemSolving/DataStructures/Graphs/graph.js
@@ -5,6 +5,7 @@ class Graph {
         this.edges = 0;
         this.adj = [];
         this.marked = [];
+        this.edgeTo = [];
         for (let i = 0; i < this.vertices; i++) {
             this.adj[i] = [];
             this.adj[i].push('');
@@ -42,6 +43,42 @@ class Graph {
             }
         }
     }
+
+    bfs(s) {
+        let queue = [];
+        this.marked[s] = true;
+        queue.push(s);
+        while (queue.length > 0) {
+            let v = queue.shift();
+            if (v !== undefined) {
+                console.log('Visited Vertex -> ', v);
+            }
+            for (let W in this.adj[v]) {
+                if (!this.marked[W]) {
+                    this.edgeTo[W] = v;
+                    this.marked[W] = true;
+                    queue.push(W);
+                }
+            }
+        }
+    }
+
+    hasPathTo(v) {
+        return this.marked[v];
+    }
+
+    pathTo(v) {
+        let source = 0;
+        if (!this.hasPathTo(v)) {
+            return 'Not possible';
+        }
+        let path = [];
+        for (let i = v; i !== source; i = this.edgeTo[i]) {
+            path.push[i];
+        }
+        path.push(source);
+        return path;
+    }
 }
 
 let graph = new Graph(5);
@@ -50,4 +87,6 @@ graph.addEdge(0, 2);
 graph.addEdge(1, 3);
 graph.addEdge(2, 4);
 graph.showGraph();
-graph.dfs(0);
+// graph.dfs(0);
+graph.bfs(0);
+console.log(graph.pathTo(4));
