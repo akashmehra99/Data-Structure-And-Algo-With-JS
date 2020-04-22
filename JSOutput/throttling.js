a33a78226d0573678f57913ab776a19b24c830a0
Akash Mehra <akash.mehra99@gmail.com>
2020-04-22 23:16:13 +0530

    Throttling in js

---
+++ JSOutput/throttling.js
@@ -0,0 +1,14 @@
+const throttle = (func, limit) => {
+    let flag = true;
+    return function () {
+        let context = this;
+        let args = arguments;
+        if (flag) {
+            func.apply(context, args);
+            flag = false;
+            setTimeout(() => {
+                flag = true;
+            }, limit);
+        }
+    }
+}
