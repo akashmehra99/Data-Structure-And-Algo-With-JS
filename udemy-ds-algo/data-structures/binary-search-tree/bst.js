95bad102103f2b1c120cd38b54fa7bdc6b21674e
Akash Mehra <akash.mehra99@gmail.com>
2022-03-21 09:04:04 +0530

    Added tree transversals

--- udemy-ds-algo/data-structures/binary-search-tree/bst.js
+++ udemy-ds-algo/data-structures/binary-search-tree/bst.js
@@ -62,12 +62,12 @@ class BinarySearchTree {
     }
 
     // Breadth first search
-    bfs() {
+    BFS() {
         let queue = [], data = [], node = this.root;
         queue.push(this.root);
         while (queue.length) {
             node = queue.shift();
-            data.push(node);
+            data.push(node.value);
             if (node.left) {
                 queue.push(node.left);
             }
@@ -77,15 +77,65 @@ class BinarySearchTree {
         }
         return data;
     }
+
+    DFSPreOrder() {
+        let data = [];
+        function traverse(node = {}) {
+            data.push(node.value);
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSPostOrder() {
+        let data = [];
+        function traverse(node = {}) {
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSInorder() {
+        let data = [];
+        function traverse(node = {}) {
+            if (node.left) {
+                traverse(node.left);
+            }
+            data.push(node.value);
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(this.root);
+        return data;
+    }
+
 }
 
 let bst = new BinarySearchTree();
 bst.insert(10);
-bst.insert(13);
+bst.insert(6);
+bst.insert(15);
+bst.insert(3);
 bst.insert(8);
-bst.insert(11);
-bst.insert(14);
-bst.insert(9);
-bst.insert(5);
-bst.bfs();
+bst.insert(20);
+
+console.log(bst.BFS());
+console.log(bst.DFSPreOrder());
+console.log(bst.DFSPostOrder());
+console.log(bst.DFSInorder());
+
 
