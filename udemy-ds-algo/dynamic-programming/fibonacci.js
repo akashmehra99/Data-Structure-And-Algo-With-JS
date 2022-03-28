6b6d70286c0505e64e68cb7e6c680ad8f5aa441f
Akash Mehra <akash.mehra99@gmail.com>
2022-03-28 09:39:04 +0530

    DP for fibonacci

---
+++ udemy-ds-algo/dynamic-programming/fibonacci.js
@@ -0,0 +1,12 @@
+function fibonacci(num, memo = []) {
+    if (memo[num]) {
+        return memo[num]
+    }
+    if (num <= 2) {
+        return 1;
+    }
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+    return memo[num];
+}
+
+console.log(fibonacci(1000))
